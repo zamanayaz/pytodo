1e59cd6c2e1cc5b0d3720f91f657a4c1ac1e589a
Zaman Ayaz <121599491+zamanayaz@users.noreply.github.com>
2024-11-13 07:38:29 +0500

    Update pytodo-cli.py
    Checks were included for invalid inputs and print error.

--- pytodo-cli.py
+++ pytodo-cli.py
@@ -1,4 +1,4 @@
-## Choices to be selected.
+ ## Choices (Options) to be selected.
 def display_menu():
     print("\nMenu: ")
     print("1. Add Task")
@@ -6,33 +6,52 @@ def display_menu():
     print("3. Mark as Done")
     print("4. View Completed Tasks")
     print("5. Exit\n")
-    
+
+# Functions to add the tasks to the list.
 def add_task(tasks):
     task = input("Enter task description: ")
-    if task = '':
+    if task == '':
         print("You have entered nothing.Please write something to add to Task List.")
         add_task(tasks)
     else:
         tasks.append(task)
         print("Task added.\n")
     
-    
+#Function to view the tasks
 def view_tasks(tasks):
-    print("\nTasks:")
-    for i, task in enumerate(tasks, start=1):
-        print(f"{i}. {task}")
-        
+    if len(tasks) >= 1:
+        print("\nTasks:")
+        for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+    else:
+        print("No Tasks to view.")
+
+#Function to  transfer completed task from mail list to completed task list.
 def mark_as_done(tasks, completed):
-    view_tasks(tasks)
-    index = int(input("Enter index number of the Task to mark as done: "))-1
-    removed_item = tasks.pop(index)
-    completed.append(removed_item)
-    
+    view_tasks(tasks), print("\n")
+    index = int(input("Enter index number of the Task to mark as done: "))
+    if index in range(1, len(tasks) + 1):
+        removed_item = tasks.pop(index - 1)
+        completed.append(removed_item)
+        print(f"The task ({index}.{removed_item}) is marked as done.")
+    else:
+        print("Index out of range.")
+        if index == 0:
+            print("Index should not be 0.")
+        elif index < 0:
+            print("Index should not be in negative.")
+        else:
+            print("Index should not exceed the length limit of tasks.")
+
+#Function to view tasks from completed list.
 def view_completed(completed):
-    for i,task in enumerate(completed, start=1):
-        print(f"{i}. {task} is completed.")
+    if len(completed) == 0:
+        print("No tasks are completed.")
+    else:
+        for i,task in enumerate(completed, start=1):
+            print(f"{i}. {task} is completed.")
         
-        
+#Main loop of the functions.
 def main():
     tasks = []
     completed = []
